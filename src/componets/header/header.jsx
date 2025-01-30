831b5c593af8b4c88b50edebabe48a78885134fe
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-30 05:43:38 +0100

    Remove navbarBrand component as it is no longer needed

--- src/componets/header/header.jsx
+++ src/componets/header/header.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import schoolLogo from "./images/oaulogo.png";
+import navbarBrand from "./navbarBrand";
+
 function Header() {
   return (
-    <nav class="navbar  fixed-top">
+    <nav class="navbar fixed-top" style={{ backgroundColor: "#aab6c4" }}>
       <div class="container-fluid">
-        <a class="navbar-brand" href="a">
+        <navbarBrand link="./#" alt="Logo" width="100" height="200" />
+        <a class="navbar-brand" href="./#">
           <img
-            src="./images/oaulogo.png"
+            src={schoolLogo}
             alt="Logo"
             width="100"
-            height="100"
-            class="d-inline-block align-text-top"
+            height="200"
+            className="d-inline-block align-text-top"
           />
         </a>
-        <div class="headingTop">
-          <h1 class="display-6">OAU Student Information Portal</h1>
-        </div>
-
+        <h5>OAU Student Information Portal</h5>
         <button
           class="navbar-toggler"
           type="button"
           data-bs-toggle="offcanvas"
-          data-bs-target="aoffcanvasNavbar"
+          data-bs-target="#offcanvasNavbar"
           aria-controls="offcanvasNavbar"
           aria-label="Toggle navigation"
         >
@@ -47,289 +48,50 @@ function Header() {
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li class="nav-item">
                 <div>
-                  <img
-                    src="./images/profile-svgrepo-com.svg"
-                    alt="person-image"
-                  />
+                  <img src={schoolLogo} alt="person-image" />
                   <h5>Lorem, ipsum dolor.</h5>
                 </div>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Profile Page
+                <a class="nav-link active" aria-current="page" href="./#">
+                  Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Health Center Registration
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Teaching Assignment
-                </a>
-              </li>
-
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="a"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Payment Activities
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Payment Via Remita
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      View Payment Status
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Download Payment Slip from Cash Office
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Complete Certificate Form
+                <a class="nav-link" href="./#">
+                  Link
                 </a>
               </li>
               <li class="nav-item dropdown">
                 <a
                   class="nav-link dropdown-toggle"
-                  href="a"
+                  href="./#"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Online Screening
+                  Dropdown
                 </a>
                 <ul class="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="a">
-                      Upload Picture
+                    <a class="dropdown-item" href="./#">
+                      Action
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="a">
-                      Upload Signature
+                    <a class="dropdown-item" href="./#">
+                      Another action
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="a">
-                      Verification Of Particulars
-                    </a>
+                    <hr class="dropdown-divider" />
                   </li>
                   <li>
-                    <a class="dropdown-item" href="a">
-                      Load Certificate Form
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Upload Scanned Certificate
-                    </a>
-                  </li>
-                  <li class="dropdown-submenu">
-                    <a
-                      class="dropdown-item dropdown-toggle"
-                      href="a"
-                      role="button"
-                    >
-                      Download
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li>
-                        <a class="dropdown-item" href="a">
-                          OAU Road Map
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="a">
-                          University Examination Regulations
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="a">
-                          Library Information
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="a">
-                          Sport Regulations
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Check Admission Status
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Activate Late Registration Payment
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Update Biodata Information
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  {" "}
-                  Register Courses{" "}
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Complete Health Centre Form
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  {" "}
-                  View Semester Raw Score
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Check Results
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Check Messages
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="a"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Bedspace Request
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Information On Accomdation
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      All Fresher BedSpace Request
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Final Year BedSpace Request
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Part 2 BedSpace Request
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Part 3 BedSpace Request
+                    <a class="dropdown-item" href="./#">
+                      Something else here
                     </a>
                   </li>
                 </ul>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="a"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Reports
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Basic Rules/Regulations in Halls of Residence
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Accomdation Aggrement Form
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      BedSpace Clerance
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Course Registration
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="a">
-                      Clearance Certificate{" "}
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Print ICT Certificate
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="a"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Documentations
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a
-                      class="dropdown-item"
-                      href="https://eportal.oauife.edu.ng/info2018_2019.pdf"
-                    >
-                      Registration Information and Fees Breakdown
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Contact Councellor
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Change Password
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="a">
-                  Sign Out
-                </a>
               </li>
             </ul>
           </div>
