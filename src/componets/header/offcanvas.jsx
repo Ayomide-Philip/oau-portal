8f3e666e5be53f0fcdd5aec098f7ffe93531bd50
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-30 23:39:19 +0100

    Add nested dropdowns for Payment Activities and Online Screening in Offcanvas component

--- src/componets/header/offcanvas.jsx
+++ src/componets/header/offcanvas.jsx
@@ -3,6 +3,9 @@ import Link from "./link";
 import schoolLogo from "./images/oaulogo.png";
 
 function Offcanvas() {
+  const handleNestedDropdownClick = (event) => {
+    event.stopPropagation(); // Prevents the parent dropdown from closing
+  };
   return (
     <div
       className="offcanvas offcanvas-end"
@@ -39,6 +42,272 @@ function Offcanvas() {
           />
 
           <Link className="nav-link" url="./#" text="Teaching Assignment" />
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="./#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Payment Activities
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Payment Via Remita
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  View Payment Status
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Download Payment Slip from Cash Office
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Complete Certificate Form
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="./#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Online Screening
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Upload Picture
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Upload Signature
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Verification Of Particulars
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Load Certificate Form
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Upload Scanned Certificate
+                </a>
+              </li>
+              <li class="dropdown-item nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="./#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  onClick={handleNestedDropdownClick}
+                >
+                  Download
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="./#">
+                      OAU Road Map
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="./#">
+                      University Examination Regulations
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="./#">
+                      Library Information
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="./#">
+                      Sport Regulations
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Check Admission Status
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Activate Late Registration Payment
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Update Biodata Information
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              {" "}
+              Register Courses{" "}
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Complete Health Centre Form
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              {" "}
+              View Semester Raw Score
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Check Results
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Check Messages
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="./#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Bedspace Request
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Information On Accomdation
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  All Fresher BedSpace Request
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Final Year BedSpace Request
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Part 2 BedSpace Request
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Part 3 BedSpace Request
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="./#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Reports
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Basic Rules/Regulations in Halls of Residence
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Accomdation Aggrement Form
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  BedSpace Clerance
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Course Registration
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="./#">
+                  Clearance Certificate
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Print ICT Certificate
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="./#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Documentations
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a
+                  class="dropdown-item"
+                  href="https://eportal.oauife.edu.ng/info2018_2019.pdf"
+                >
+                  Registration Information and Fees Breakdown
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Contact Councellor
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Change Password
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./#">
+              Sign Out
+            </a>
+          </li>
         </ul>
       </div>
     </div>
