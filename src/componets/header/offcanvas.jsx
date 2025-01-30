dd03f4a5fb025ab44afd84c63c0d5715c79ecd1c
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 00:48:59 +0100

    Refactor Offcanvas component to integrate OnlineScreening for improved document handling

--- src/componets/header/offcanvas.jsx
+++ src/componets/header/offcanvas.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import profile from "./images/profile-svgrepo-com.svg";
 import List from "./lists";
 import PaymentMethodDropdown from "./paymentMethodDropdown";
+import OnlineScreening from "./onlineScreening";
 function Offcanvas() {
-  const handleNestedDropdownClick = (event) => {
-    event.stopPropagation(); // Prevents the parent dropdown from closing
-  };
   return (
     <div
       className="offcanvas offcanvas-end"
@@ -63,78 +61,7 @@ function Offcanvas() {
             text="Complete Certificate Form"
           />
 
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="./#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Online Screening
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Upload Picture
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Upload Signature
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Verification Of Particulars
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Load Certificate Form
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Upload Scanned Certificate
-                </a>
-              </li>
-              <li className="dropdown-item nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="./#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                  onClick={handleNestedDropdownClick}
-                >
-                  Download
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="./#">
-                      OAU Road Map
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="./#">
-                      University Examination Regulations
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="./#">
-                      Library Information
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="./#">
-                      Sport Regulations
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
+          <OnlineScreening />
 
           <List
             listClassName="nav-item"
