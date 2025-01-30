666d6f5a442c0ec0332512b672221be9d51d5cd8
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-30 23:46:24 +0100

    Refactor Offcanvas component to use className instead of class for JSX compatibility

--- src/componets/header/offcanvas.jsx
+++ src/componets/header/offcanvas.jsx
@@ -43,9 +43,9 @@ function Offcanvas() {
 
           <Link className="nav-link" url="./#" text="Teaching Assignment" />
 
-          <li class="nav-item dropdown">
+          <li className="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle"
+              className="nav-link dropdown-toggle"
               href="./#"
               role="button"
               data-bs-toggle="dropdown"
@@ -53,33 +53,33 @@ function Offcanvas() {
             >
               Payment Activities
             </a>
-            <ul class="dropdown-menu">
+            <ul className="dropdown-menu">
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Payment Via Remita
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   View Payment Status
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Download Payment Slip from Cash Office
                 </a>
               </li>
             </ul>
           </li>
 
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Complete Certificate Form
             </a>
           </li>
-          <li class="nav-item dropdown">
+          <li className="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle"
+              className="nav-link dropdown-toggle"
               href="./#"
               role="button"
               data-bs-toggle="dropdown"
@@ -87,35 +87,35 @@ function Offcanvas() {
             >
               Online Screening
             </a>
-            <ul class="dropdown-menu">
+            <ul className="dropdown-menu">
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Upload Picture
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Upload Signature
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Verification Of Particulars
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Load Certificate Form
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Upload Scanned Certificate
                 </a>
               </li>
-              <li class="dropdown-item nav-item dropdown">
+              <li className="dropdown-item nav-item dropdown">
                 <a
-                  class="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle"
                   href="./#"
                   role="button"
                   data-bs-toggle="dropdown"
@@ -124,24 +124,24 @@ function Offcanvas() {
                 >
                   Download
                 </a>
-                <ul class="dropdown-menu">
+                <ul className="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="./#">
+                    <a className="dropdown-item" href="./#">
                       OAU Road Map
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="./#">
+                    <a className="dropdown-item" href="./#">
                       University Examination Regulations
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="./#">
+                    <a className="dropdown-item" href="./#">
                       Library Information
                     </a>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="./#">
+                    <a className="dropdown-item" href="./#">
                       Sport Regulations
                     </a>
                   </li>
@@ -149,51 +149,51 @@ function Offcanvas() {
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Check Admission Status
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Activate Late Registration Payment
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Update Biodata Information
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               {" "}
               Register Courses{" "}
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Complete Health Centre Form
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               {" "}
               View Semester Raw Score
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Check Results
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Check Messages
             </a>
           </li>
-          <li class="nav-item dropdown">
+          <li className="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle"
+              className="nav-link dropdown-toggle"
               href="./#"
               role="button"
               data-bs-toggle="dropdown"
@@ -201,37 +201,37 @@ function Offcanvas() {
             >
               Bedspace Request
             </a>
-            <ul class="dropdown-menu">
+            <ul className="dropdown-menu">
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Information On Accomdation
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   All Fresher BedSpace Request
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Final Year BedSpace Request
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Part 2 BedSpace Request
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Part 3 BedSpace Request
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item dropdown">
+          <li className="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle"
+              className="nav-link dropdown-toggle"
               href="./#"
               role="button"
               data-bs-toggle="dropdown"
@@ -239,42 +239,42 @@ function Offcanvas() {
             >
               Reports
             </a>
-            <ul class="dropdown-menu">
+            <ul className="dropdown-menu">
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Basic Rules/Regulations in Halls of Residence
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Accomdation Aggrement Form
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   BedSpace Clerance
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Course Registration
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="./#">
+                <a className="dropdown-item" href="./#">
                   Clearance Certificate
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Print ICT Certificate
             </a>
           </li>
-          <li class="nav-item dropdown">
+          <li className="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle"
+              className="nav-link dropdown-toggle"
               href="./#"
               role="button"
               data-bs-toggle="dropdown"
@@ -282,10 +282,10 @@ function Offcanvas() {
             >
               Documentations
             </a>
-            <ul class="dropdown-menu">
+            <ul className="dropdown-menu">
               <li>
                 <a
-                  class="dropdown-item"
+                  className="dropdown-item"
                   href="https://eportal.oauife.edu.ng/info2018_2019.pdf"
                 >
                   Registration Information and Fees Breakdown
@@ -293,18 +293,18 @@ function Offcanvas() {
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Contact Councellor
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Change Password
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="./#">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="./#">
               Sign Out
             </a>
           </li>
