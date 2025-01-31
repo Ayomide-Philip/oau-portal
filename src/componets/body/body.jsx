68ffd9f4c50ff5f5325f1908f6c0455d752e55d2
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 02:24:17 +0100

    Add detailed user information display in Body component

--- src/componets/body/body.jsx
+++ src/componets/body/body.jsx
@@ -3,14 +3,37 @@ import React from "react";
 function Body() {
   return (
     <div className="main-container container-xl">
-      <div className="image-thumbnail">
+      <div className="rounded-2">
         <img
           src="/images/oaulogo.png"
-          className="rounded img-thumbnail float-end"
+          className="float-end"
           alt="..."
           height="200px"
           width="200px"
         ></img>
+      </div>
+      <div>
+        <p className="display-6 fs-4">Registration Number: 20241234567JF</p>
+        <p className="display-6 fs-4">Name: Test Website</p>
+        <p className="display-6 fs-4">Current Part: 1</p>
+        <p className="display-6 fs-4">
+          Degree Programme: B.Sc Computer Science with Mathematics
+        </p>
+        <p className="display-6 fs-4">
+          Department: Computer Science and Cyber Security
+        </p>
+        <p className="display-6 fs-4">
+          Faculty: Computing Science and Engineering
+        </p>
+        <p className="display-6 fs-4">
+          BedSpace Location: Angola Hall, Block E1, Ground Floor, Room 106, Bed
+          No 1 (Lower Bunk)
+        </p>
+        <p className="display-6 fs-4">Email Address:</p>
+        <p className="display-6 fs-4">
+          Your Matric/Registration Number,( the alphabets in CAPSLOCK):
+          202440604235JF
+        </p>
       </div>
     </div>
   );
