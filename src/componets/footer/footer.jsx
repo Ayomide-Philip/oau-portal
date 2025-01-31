8e91d7978d9e6b415a24b7ffab4cfbe850d9fb2a
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 03:05:16 +0100

    Refactor Footer component structure for improved readability and layout

--- src/componets/footer/footer.jsx
+++ src/componets/footer/footer.jsx
@@ -3,49 +3,54 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <div class="container text-start">
-        <div class="row align-items-start">
-          <div class="col">
-            {" "}
-            <a href="./#">Privacy Statement</a>
+      <div className="container">
+        <div class=" text-start">
+          <div class="row align-items-start">
+            <div class="col">
+              <a href="./#">Privacy Statement</a>
+            </div>
+            <div class="col">
+              <a href="./#">Examination Time Table</a>
+            </div>
           </div>
-          <div class="col">
-            <a href="./#">Examination Time Table</a>
+
+          <div class="row align-items-start">
+            <div class="col">
+              <a href="./#">Terms and Conditions of Use</a>
+            </div>
+            <div class="col">
+              <a href="./#"> Contact Us</a>
+            </div>
+          </div>
+
+          <div class="row align-items-start">
+            <div class="col">
+              <a href="./#">Legal Notice</a>
+            </div>
+            <div class="col">
+              <a href="./#"> Student Help</a>
+            </div>
+          </div>
+
+          <div class="row align-items-start">
+            <div class="col">
+              <a href="./#">FAQ</a>
+            </div>
+            <div class="col">
+              <a href="./#"> Staff Help</a>
+            </div>
+          </div>
+
+          <div class="row align-items-start">
+            <div class="col">
+              <a href="./#">Lecture Time Table</a>
+            </div>
           </div>
         </div>
 
-        <div class="row align-items-start">
-          <div class="col">
-            <a href="./#">Terms and Conditions of Use</a>
-          </div>
-          <div class="col">
-            <a href="./#"> Contact Us</a>
-          </div>
-        </div>
-
-        <div class="row align-items-start">
-          <div class="col">
-            <a href="./#">Legal Notice</a>
-          </div>
-          <div class="col">
-            <a href="./#"> Student Help</a>
-          </div>
-        </div>
-
-        <div class="row align-items-start">
-          <div class="col">
-            <a href="./#">FAQ</a>
-          </div>
-          <div class="col">
-            <a href="./#"> Staff Help</a>
-          </div>
-        </div>
-
-        <div class="row align-items-start">
-          <div class="col">
-            <a href="./#">Lecture Time Table</a>
-          </div>
-        </div>
+        <p className="lead">
+          &copy; 2006 - {new Date().getFullYear()} Obafemi Awolowo University
+        </p>
       </div>
     </footer>
   );
