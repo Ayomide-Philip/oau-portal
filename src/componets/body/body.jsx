1bb3dcf6da3a076103910ab86c7fa7e08b37ed56
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 03:25:36 +0100

    Refactor Body component to use Info subcomponent for improved code organization and readability

--- src/componets/body/body.jsx
+++ src/componets/body/body.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Info from "./info";
 function Body() {
   return (
     <div className="main-container container-xl">
@@ -14,27 +14,21 @@ function Body() {
       </div>
       <div>
         <p className="display-4">Student Details</p>
-        <p className="display-6 fs-4">Registration Number: 20241234567JF</p>
-        <p className="display-6 fs-4">Name: Areo Ayomide Philip</p>
-        <p className="display-6 fs-4">Current Part: 1</p>
-        <p className="display-6 fs-4">
-          Degree Programme: B.Sc Computer Science with Mathematics
-        </p>
-        <p className="display-6 fs-4">
-          Department: Computer Science and Cyber Security
-        </p>
-        <p className="display-6 fs-4">
-          Faculty: Computing Science and Engineering
-        </p>
-        <p className="display-6 fs-4">
-          BedSpace Location: Angola Hall, Block E1, Ground Floor, Room 106, Bed
-          No 1 (Lower Bunk)
-        </p>
-        <p className="display-6 fs-4">Email Address:</p>
-        <p className="display-6 fs-4">
-          Your Matric/Registration Number,( the alphabets in CAPSLOCK):
-          202440604235JF
-        </p>
+        <Info q="Registration Number" a="20241234567JF" />
+        <Info q="Name" a="Areo Ayomide Philip" />
+        <Info q="Current Part" a="1" />
+        <Info q="Degree Programme" a="B.Sc Computer Science with Mathematics" />
+        <Info q="Department" a="Computer Science and Cyber Security" />
+        <Info q="Faculty" a="Computing Science and Engineering" />
+        <Info
+          q="BedSpace Location"
+          a="Angola Hall, Block E1, Ground Floor, Room 106, Bed No 1 (Lower Bunk)"
+        />
+        <Info q="Email Address" a="" />
+        <Info
+          q="Your Matric/Registration Number,( the alphabets in CAPSLOCK)"
+          a="202440604235JF"
+        />
       </div>
     </div>
   );
