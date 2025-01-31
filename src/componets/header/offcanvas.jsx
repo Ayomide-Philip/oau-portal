59666f4a8d59376bde1d8a4d4391b68e3adeacd1
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 01:23:43 +0100

    Remove deprecated Reports dropdown in Offcanvas for cleaner navigation

--- src/componets/header/offcanvas.jsx
+++ src/componets/header/offcanvas.jsx
@@ -123,44 +123,6 @@ function Offcanvas() {
           <BedspaceDropdown />
 
           <ReportsDropdown />
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="./#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Reports
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Basic Rules/Regulations in Halls of Residence
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Accomdation Aggrement Form
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  BedSpace Clerance
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Course Registration
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="./#">
-                  Clearance Certificate
-                </a>
-              </li>
-            </ul>
-          </li>
 
           <List
             listClassName="nav-item"
