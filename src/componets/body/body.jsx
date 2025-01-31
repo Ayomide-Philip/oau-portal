ae2002f7c6091418a87a939a395766f79cb91589
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 02:11:15 +0100

    Add Body component with image thumbnail for enhanced layout

--- src/componets/body/body.jsx
+++ src/componets/body/body.jsx
@@ -1,2 +1,19 @@
 import React from "react";
 
+function Body() {
+  return (
+    <div className="main-container container-xl">
+      <div className="image-thumbnail">
+        <img
+          src="/images/oaulogo.png"
+          className="rounded img-thumbnail float-end"
+          alt="..."
+          height="200px"
+          width="200px"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
