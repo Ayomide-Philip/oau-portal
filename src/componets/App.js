8f67ce288339a8ef0062f0faf03f93f83f17c251
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 02:11:08 +0100

    Add Body component to App for improved application structure

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./header/header.jsx";
-
+import Body from "./body/body.jsx";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
