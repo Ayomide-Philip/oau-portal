6586a450b21efe706d8d62c654b0915b6712a1e1
Areo Ayomide <areoayomide2008@gmail.com>
2025-01-31 02:49:55 +0100

    Add Footer component to App for improved layout

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header/header.jsx";
 import Body from "./body/body.jsx";
+import Footer from "./footer/footer.jsx";
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
