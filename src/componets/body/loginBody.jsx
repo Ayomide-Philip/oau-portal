bb731fc5f257981b3768863a00ba272e28347dfc
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 22:14:11 +0100

    Refactor LoginBody component to simplify image rendering

--- src/componets/body/loginBody.jsx
+++ src/componets/body/loginBody.jsx
@@ -4,11 +4,7 @@ function LoginBody() {
   return (
     <div className="main-container">
       <div class="card mx-auto my-5">
-        <img
-          src="/images/oaulogo.png"
-          class="card-img-top"
-          alt="School Logo"
-        />
+        <img src="/images/oaulogo.png" class="card-img-top" alt="School Logo" />
         <div class="card-body">
           <h5 class="card-title display-6 text-center">Login</h5>
           <a href="./#" class="btn btn-primary">
