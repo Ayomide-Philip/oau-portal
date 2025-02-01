91062c3d2519de6840911e66dacb7f0cc64c89fb
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 19:49:09 +0100

    Add HealthRegistration component with Header and Footer

---
+++ src/pages/healthregistration.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../componets/header/header";
+import Footer from "../componets/footer/footer";
+function HealthRegistration() {
+  return (
+    <>
+      <Header />
+      <div className="main-container">
+        <h1>Health Registration</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default HealthRegistration;
