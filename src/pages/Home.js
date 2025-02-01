e2d412029aecfa6cc7eba28c21d925b02293f1ef
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 19:49:57 +0100

    Replace Body component with HomeBody in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../componets/header/header.jsx";
-import Body from "../componets/body/body.jsx";
+import HomeBody from "../componets/body/homeBody.jsx";
 import Footer from "../componets/footer/footer.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body /> 
+      <HomeBody />
       <Footer />
     </div>
   );
