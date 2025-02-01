92921b0ff0d6279c7c6c8c5ebcecebb0ec71b015
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 21:46:44 +0100

    Add LoginBody component to Login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../componets/header/header";
 import Footer from "../componets/footer/footer";
+import LoginBody from "../componets/body/loginBody";
 
 function Login() {
   return (
     <>
       <Header />
+      <LoginBody />
       <Footer />
     </>
   );
