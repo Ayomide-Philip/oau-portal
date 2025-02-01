31c69b3235e98683904f50f79c8712f39221cda3
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 21:43:05 +0100

    Add Login component with Header and Footer

---
+++ src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../componets/header/header";
+import Footer from "../componets/footer/footer";
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Login;
