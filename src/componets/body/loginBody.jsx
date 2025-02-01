5a5115b6fb8ca68418db8d5dd5e910a000bfb169
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 21:59:23 +0100

    Enhance LoginBody component with card layout and content

--- src/componets/body/loginBody.jsx
+++ src/componets/body/loginBody.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 function LoginBody() {
-    return(
-        <div className="main-container"></div>
-    )
+  return (
+    <div className="main-container">
+      <div class="card mx-auto my-5" style={{ width: "20rem" }}>
+        <img src="/images/oaulogo.png" class="card-img-top" alt="School Logo" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="./#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginBody;
