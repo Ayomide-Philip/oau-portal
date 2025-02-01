b5d1398ad3d49d09bb9f2cb589b19723ef5b0d5a
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 12:24:01 +0100

    Add Home component with Header, Body, and Footer structure

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../componets/header/header.jsx";
+import Body from "../componets/body/body.jsx";
+import Footer from "../componets/footer/footer.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body /> 
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
