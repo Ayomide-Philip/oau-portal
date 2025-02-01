458dabfd33331190e73dbeca7e192bc971754d70
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-01 21:40:12 +0100

    Update routing to set Login as the default page and add home route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import App from "./pages/Home";
 import HealthRegistration from "./pages/healthregistration";
+import Login from "./pages/login";
 
 function AppRouting() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<App />} />
       <Route path="/healthRegistration" element={<HealthRegistration />} />
     </Routes>
   );
