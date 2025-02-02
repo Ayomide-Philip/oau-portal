733a79cad08fe911d1429b17a7dededfa3d844a5
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-02 21:47:39 +0100

    Refactor LoginBody component to use JSX syntax and improve accessibility

--- src/componets/body/loginBody.jsx
+++ src/componets/body/loginBody.jsx
@@ -3,65 +3,69 @@ import React from "react";
 function LoginBody() {
   return (
     <div className="main-container">
-      <div class="card mx-auto my-5">
-        <img src="/images/oaulogo.png" class="card-img-top" alt="School Logo" />
-        <div class="card-body">
-          <h5 class="card-title display-6 text-center">Login</h5>
-          <div class="input-group mb-3">
-            <span class="input-group-text">
+      <div className="card mx-auto my-5">
+        <img
+          src="/images/oaulogo.png"
+          className="card-img-top"
+          alt="School Logo"
+        />
+        <div className="card-body">
+          <h5 className="card-title display-6 text-center">Login</h5>
+          <div className="input-group mb-3">
+            <span className="input-group-text">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-person-fill"
+                className="bi bi-person-fill"
                 viewBox="0 0 16 16"
               >
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
               </svg>
             </span>
-            <div class="form-floating">
+            <div className="form-floating">
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="utmeMatricNumber"
                 placeholder="UTME/MATRIC No."
               />
-              <label for="utmeMatricNumber">UTME/MATRIC No.</label>
+              <label htmlFor="utmeMatricNumber">UTME/MATRIC No.</label>
             </div>
           </div>
-          <div class="input-group mb-3">
-            <span class="input-group-text">
+          <div className="input-group mb-3">
+            <span className="input-group-text">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-shield-lock-fill"
+                className="bi bi-shield-lock-fill"
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5"
                 />
               </svg>
             </span>
-            <div class="form-floating">
+            <div className="form-floating">
               <input
                 type="password"
-                class="form-control"
+                className="form-control"
                 id="passswordSurname"
                 placeholder="Passsword/Surname"
               />
-              <label for="passswordSurname">Passsword/Surname</label>
+              <label htmlFor="passswordSurname">Passsword/Surname</label>
             </div>
-            <span class="input-group-text" id="viewPassword">
+            <span className="input-group-text" id="viewPassword">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-eye-fill"
+                className="bi bi-eye-fill"
                 viewBox="0 0 16 16"
               >
                 <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
@@ -69,8 +73,8 @@ function LoginBody() {
               </svg>
             </span>
           </div>
-          <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-primary" type="button">
+          <div className="d-grid gap-2 col-6 mx-auto">
+            <button className="btn btn-primary" type="button">
               Login
             </button>
           </div>
