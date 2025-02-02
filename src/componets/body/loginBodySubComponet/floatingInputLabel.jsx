fc504520c1bf5964cbedf8d79e1d373d0bbbf6af
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-03 00:15:55 +0100

    Fix FloatingInputLabel component to return JSX correctly

--- src/componets/body/loginBodySubComponet/floatingInputLabel.jsx
+++ src/componets/body/loginBodySubComponet/floatingInputLabel.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 function FloatingInputLabel(props) {
-  <div className="form-floating">
-    <input
-      type={props.type}
-      className="form-control"
-      id={props.id}
-      placeholder={props.label}
-    />
-    <label htmlFor={props.id}>{props.label}</label>
-  </div>;
+  return (
+    <div className="form-floating">
+      <input
+        type={props.type}
+        className="form-control"
+        id={props.id}
+        placeholder={props.label}
+      />
+      <label htmlFor={props.id}>{props.label}</label>
+    </div>
+  );
 }
 
 export default FloatingInputLabel;
