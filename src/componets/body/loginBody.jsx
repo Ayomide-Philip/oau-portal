3f83813e3c2ef99d322fa0edb22122e01bcc4bf5
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-03 00:15:44 +0100

    Add FloatingInputLabel component to LoginBody for enhanced input labeling

--- src/componets/body/loginBody.jsx
+++ src/componets/body/loginBody.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import FloatingInputLabel from "./loginBodySubComponet/floatingInputLabel";
 function LoginBody() {
   return (
     <div className="main-container">
@@ -24,6 +24,11 @@ function LoginBody() {
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
               </svg>
             </span>
+            <FloatingInputLabel
+              type="text"
+              id="utmeMatricNumber"
+              label="UTME/MATRIC No."
+            />
             <div className="form-floating">
               <input
                 type="text"
