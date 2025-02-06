026d47f063d8b6038242cea138ed3c358234dccc
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-06 23:10:54 +0100

    Add HrsBody component to HealthRegistration: integrate body structure into the layout

--- src/pages/healthregistration.jsx
+++ src/pages/healthregistration.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../componets/header/header";
 import Footer from "../componets/footer/footer";
+import HrsBody from "../componets/body/hrsBody";
 function HealthRegistration() {
   return (
     <>
       <Header />
       <div className="main-container">
-        <h1>Health Registration</h1>
+      
       </div>
       <Footer />
     </>
