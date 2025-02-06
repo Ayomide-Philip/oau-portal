b80e06ee8237bbe627ecc5a1329db07a893f735f
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-06 23:12:40 +0100

    Add header to HrsBody component: include title for Health Registration

--- src/componets/body/hrsBody.jsx
+++ src/componets/body/hrsBody.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HrsBody() {
     return(
         <div className="main-container">
-            
+              <h1>Health Registration</h1>
         </div>
     )
 }
