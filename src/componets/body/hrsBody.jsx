264823ce75b041b357afa3305983645297471e84
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-07 21:30:37 +0100

    Update HrsBody component: center align the title for Health Registration

--- src/componets/body/hrsBody.jsx
+++ src/componets/body/hrsBody.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function HrsBody() {
-    return(
-        <div className="main-container">
-              <h1>Health Registration</h1>
-        </div>
-    )
+  return (
+    <div className="main-container">
+      <h1 className="text-center">Health Registration</h1>
+    </div>
+  );
 }
 
 export default HrsBody;
