3f80c15920f6220a649484213b51475a4b46eff6
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-08 02:42:02 +0100

    Update LoginBody component: add background image and improve input field structure

--- src/componets/body/loginBody.jsx
+++ src/componets/body/loginBody.jsx
@@ -24,130 +24,136 @@ function LoginBody() {
 
   return (
     <div className="main-container">
-      <div className="card mx-auto my-5">
-        <img
-          src="/images/oaulogo.png"
-          className="card-img-top"
-          alt="School Logo"
-        />
-        <div className="card-body">
-          <h5 className="card-title lead text-center fw-bold">
-            {name === "" || undefined ? "Hello Guest" : `Hello ${name}`}
-          </h5>
-          <form>
-            <div className="input-group mb-3">
-              <span className="input-group-text">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-person-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                </svg>
-              </span>
-              <FloatingInputLabel
-                type="text"
-                id="utmeMatricNumber"
-                label="UTME/MATRIC No."
-                func={() => {
-                  setUsername(
-                    document.getElementById("utmeMatricNumber").value
-                  );
-                }}
-              />
-            </div>
-            <div className="input-group mb-3">
-              <span className="input-group-text">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-shield-lock-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5"
-                  />
-                </svg>
-              </span>
-              <FloatingInputLabel
-                type={isVisible ? "text" : "password"}
-                id="passswordSurname"
-                label="Passsword/Surname"
-              />
-              <button
-                type="button"
-                onClick={() => {
-                  setIsVisible(!isVisible);
-                }}
-                className="input-group-text"
-                id="viewPassword"
-              >
-                {isVisible ? (
+      <div className="background-image">
+        <div className="card mx-auto my-5">
+          <img
+            src="/images/oaulogo.png"
+            className="card-img-top"
+            alt="School Logo"
+          />
+          <div className="card-body">
+            <h5 className="card-title lead text-center fw-bold">
+              {name === "" || undefined ? "Hello Guest" : `Hello ${name}`}
+            </h5>
+            <form>
+              <div className="input-group mb-3">
+                <span className="input-group-text">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-eye-slash"
+                    className="bi bi-person-fill"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
-                    <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
-                    <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                   </svg>
-                ) : (
+                </span>
+                <FloatingInputLabel
+                  type="text"
+                  id="utmeMatricNumber"
+                  label="UTME/MATRIC No."
+                  func={() => {
+                    setUsername(
+                      document.getElementById("utmeMatricNumber").value
+                    );
+                  }}
+                />
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-eye"
+                    className="bi bi-shield-lock-fill"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                    <path
+                      fillRule="evenodd"
+                      d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5"
+                    />
                   </svg>
-                )}
-              </button>
-            </div>
-            <div className="input-group mb-3">
-              <select
-                defaultValue={new Date().getFullYear() - 2}
-                class="form-select"
-                aria-label="Session Selection"
-                name="session"
-              >
-                <option>Select Session</option>
-                {year.map((years) => {
-                  return (
-                    <option value={years.session} key={years.session}>
-                      {years.year}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className="input-group mb-3">
-              <select
-                class="form-select"
-                aria-label="Semester Period"
-                name="semesterPeriod"
-              >
-                <option>Select Semester</option>
-                <option value="Harmattan">Harmattan</option>;
-                <option value="Rain">Rain</option>;
-              </select>
-            </div>
-            <div className="d-grid gap-2">
-              <input type="submit" className="btn btn-primary" value="Login" />
-            </div>
-          </form>
+                </span>
+                <FloatingInputLabel
+                  type={isVisible ? "text" : "password"}
+                  id="passswordSurname"
+                  label="Passsword/Surname"
+                />
+                <button
+                  type="button"
+                  onClick={() => {
+                    setIsVisible(!isVisible);
+                  }}
+                  className="input-group-text"
+                  id="viewPassword"
+                >
+                  {isVisible ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-eye-slash"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
+                      <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
+                      <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-eye"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                    </svg>
+                  )}
+                </button>
+              </div>
+              <div className="input-group mb-3">
+                <select
+                  defaultValue={new Date().getFullYear() - 2}
+                  class="form-select"
+                  aria-label="Session Selection"
+                  name="session"
+                >
+                  <option>Select Session</option>
+                  {year.map((years) => {
+                    return (
+                      <option value={years.session} key={years.session}>
+                        {years.year}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <div className="input-group mb-3">
+                <select
+                  class="form-select"
+                  aria-label="Semester Period"
+                  name="semesterPeriod"
+                >
+                  <option>Select Semester</option>
+                  <option value="Harmattan">Harmattan</option>;
+                  <option value="Rain">Rain</option>;
+                </select>
+              </div>
+              <div className="d-grid gap-2">
+                <input
+                  type="submit"
+                  className="btn btn-primary"
+                  value="Login"
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
