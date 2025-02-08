b3dc1329491e21a517f288d6d94f2d94f01211ca
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-08 02:29:14 +0100

    Update Menubutton component: enhance styling with dark theme support and button outline

--- src/componets/header/menubutton.jsx
+++ src/componets/header/menubutton.jsx
@@ -3,15 +3,18 @@ import React from "react";
 function Menubutton() {
   return (
     <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="offcanvas"
-      data-bs-target="#offcanvasNavbar"
-      aria-controls="offcanvasNavbar"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    className="navbar-toggler btn-outline-primary btn"
+    type="button"
+    data-bs-toggle="offcanvas"
+    data-bs-target="#offcanvasNavbar"
+    aria-controls="offcanvasNavbar"
+    aria-label="Toggle navigation"
+    data-cs-theme="dark"
+    style={{ backgroundColor: 'white', color: 'black' }}
+  >
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  
   );
 }
 
