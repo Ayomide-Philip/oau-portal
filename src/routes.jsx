2a66cbf6c31fa5b18d38dc3196c64d14c8cc1a42
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-10 20:31:50 +0100

    Add route for Home component

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import App from "./pages/profile";
 import HealthRegistration from "./pages/healthregistration";
 import Login from "./pages/login";
+import Home from "./pages/home";
 
 function AppRouting() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<App />} />
       <Route path="/healthRegistration" element={<HealthRegistration />} />
