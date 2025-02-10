76c284fe19ab6e9a4b69ccdf85a883dfac09b1c3
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-10 20:32:11 +0100

    Add Home component with HomeHeader integration

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HomeHeader from "../componets/home/homeHeader";
+function Home() {
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
+}
+
+export default Home;
