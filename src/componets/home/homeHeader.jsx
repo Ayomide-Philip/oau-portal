85fd5f079ee60d5c19721606c2a4fb1db16c8eed
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-10 21:00:49 +0100

    Update HomeHeader component to use relative links for navigation

--- src/componets/home/homeHeader.jsx
+++ src/componets/home/homeHeader.jsx
@@ -22,7 +22,7 @@ function HomeHeader() {
           class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          data-bs-target="./#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -33,19 +33,19 @@ function HomeHeader() {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+              <a class="nav-link active" aria-current="page" href="./#">
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="./#">
                 Link
               </a>
             </li>
             <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
-                href="#"
+                href="./#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
@@ -54,12 +54,12 @@ function HomeHeader() {
               </a>
               <ul class="dropdown-menu">
                 <li>
-                  <a class="dropdown-item" href="#">
+                  <a class="dropdown-item" href="./#">
                     Action
                   </a>
                 </li>
                 <li>
-                  <a class="dropdown-item" href="#">
+                  <a class="dropdown-item" href="./#">
                     Another action
                   </a>
                 </li>
@@ -67,7 +67,7 @@ function HomeHeader() {
                   <hr class="dropdown-divider" />
                 </li>
                 <li>
-                  <a class="dropdown-item" href="#">
+                  <a class="dropdown-item" href="./#">
                     Something else here
                   </a>
                 </li>
