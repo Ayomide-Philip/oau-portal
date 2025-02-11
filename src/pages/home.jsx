8ea8f96badf3791287d8dc1ff31e4602ac718954
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-11 09:55:51 +0100

    Set background image height to 100vh for improved layout on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <HomeHeader />
-      <div className="background-image" >
+      <div className="background-image" style={{ height: "100vh" }}>
         <div class="text-center background container-fluid">
           <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2">
             <div class="col-md-4 mt-3 mb-3">
