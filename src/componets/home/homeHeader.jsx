f983733a48a127e76900743f59e7dadab0572f8d
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-11 01:01:28 +0100

    Replace SVG icon with image for home in header and update alt text for accessibility

--- src/componets/home/homeHeader.jsx
+++ src/componets/home/homeHeader.jsx
@@ -41,16 +41,14 @@ function HomeHeader() {
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="./#">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  fill="currentColor"
-                  class="bi bi-house-door-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
-                </svg>
+                <img
+                  src="/images/home-1-svgrepo-com.svg"
+                  alt=""
+                  width="40px"
+                  height="30px"
+                  className="d-none d-lg-block"
+                />
+                <span className="d-inline d-lg-none">Home</span>
               </a>
             </li>
             <li class="nav-item">
@@ -60,6 +58,7 @@ function HomeHeader() {
                   width="40"
                   height="30"
                   alt="graduate-1-svgrepo-com"
+                  className="d-none d-lg-block"
                 />
               </a>
             </li>
@@ -76,6 +75,7 @@ function HomeHeader() {
                   width="40"
                   height="30"
                   alt=""
+                  className="d-none d-lg-block"
                 />
               </a>
               <ul class="dropdown-menu">
@@ -113,7 +113,7 @@ function HomeHeader() {
               <a class="nav-link" href="./#">
                 <img
                   src="/images/more-horizontal-circle-svgrepo-com.svg"
-                  alt="contact-writing-write-svgrepo-com"
+                  alt="more-horizontal-circle-svgrepo-com"
                   width="40"
                   height="30"
                 />
