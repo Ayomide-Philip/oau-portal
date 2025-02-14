6ac8af90807371950063d50306a9e9ff3f1bb401
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-14 21:53:19 +0100

    feat: add HomeBody component for main content display

---
+++ src/componets/home/homeBody.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function HomeBody() {
+  return (
+    <div className="background-image1 background-image">
+      <div class="text-center background container-fluid">
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2">
+          <div class="col-md-4 mt-3 mb-3">
+            <img
+              src="/images/oaulogo.png"
+              alt="oaulogo"
+              width="250px"
+              height="250px"
+            />
+          </div>
+          <div class="col-md-8 mt-3 mb-3">
+            <div className="h1 heading-5" style={{ color: "#FECD4A" }}>
+              OBAFEMI AWOLOWO UNIVERSITY
+            </div>
+            <div className="h3" style={{ color: "white" }}>
+              (STUDENT INFORMATION PORTAL)
+            </div>
+            <div className="h1 heading-4" style={{ color: "#FECD4A" }}>
+              Welcome
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeBody;
