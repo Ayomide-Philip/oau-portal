9d56473d41a07c792ab8001f7e186d8e2ddaf40c
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-14 21:53:26 +0100

    feat: replace static content in Home component with HomeBody component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,36 +1,12 @@
 import React from "react";
 import HomeHeader from "../componets/home/homeHeader";
 import HomeFooter from "../componets/home/footer";
-
+import HomeBody from "../componets/home/homeBody";
 function Home() {
   return (
     <>
       <HomeHeader />
-      <div className="background-image1 background-image">
-        <div class="text-center background container-fluid">
-          <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2">
-            <div class="col-md-4 mt-3 mb-3">
-              <img
-                src="/images/oaulogo.png"
-                alt="oaulogo"
-                width="250px"
-                height="250px"
-              />
-            </div>
-            <div class="col-md-8 mt-3 mb-3">
-              <div className="h1 heading-5" style={{ color: "#FECD4A" }}>
-                OBAFEMI AWOLOWO UNIVERSITY
-              </div>
-              <div className="h3" style={{ color: "white" }}>
-                (STUDENT INFORMATION PORTAL)
-              </div>
-              <div className="h1 heading-4" style={{ color: "#FECD4A" }}>
-                Welcome
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <HomeBody />
       <HomeFooter />
     </>
   );
