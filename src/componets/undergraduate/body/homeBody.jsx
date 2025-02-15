5f352534a0d0820220d1eb12c0933be91cfbc2d2
Areo Ayomide <areoayomide2008@gmail.com>
2025-02-15 01:02:37 +0100

    fix: update HomeBody component to display personal information with improved layout

--- src/componets/undergraduate/body/homeBody.jsx
+++ src/componets/undergraduate/body/homeBody.jsx
@@ -2,8 +2,37 @@ import React from "react";
 
 function HomeBody() {
   return (
-    <div className="main-container container-xl">
-      <h1>Welcome Areo Ayomide Philip</h1>
+    <div
+      className="main-container container-xl"
+      style={{ marginBottom: "50px" }}
+    >
+      <div
+        class="container text-center"
+        style={{
+          backgroundColor: "white",
+          padding: "10px",
+          borderRadius: "10px",
+        }}
+      >
+        <h2>Personal Information</h2>
+        <div class="row">
+          <div class="col-sm-4">
+            <img
+              src="/images/profile-svgrepo-com.svg"
+              alt="profile"
+              width="200px"
+              height="200px"
+              className="profile-picture"
+            />
+          </div>
+          <div class="col-sm-8 text-start">
+            <div class="row">
+              <div class="col-4">1 of 3</div>
+              <div class="col-8">2 of 3 (wider)</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
